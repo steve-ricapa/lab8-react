5e4db59005ee9fd6bdbb0499ac94629c3b8209fd
steve-ricapa <steve.ricapa@gmail.com>
2025-06-16 18:18:52 -0500

    1st commit helloworld

---
+++ src/components/CourseList.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function CourseList() {
+  return (
+    <>
+        <h1 className="text-xl">Course List</h1>
+        <ul>
+            <li>DBP</li>
+            <li>ADA</li>
+        </ul>
+    </>
+  )
+}
+
+export default CourseList
